71e83ac9218167d3d080358f784ce2a0dc794d02
Lazar Petrovic <lazarp227@gmail.com>
2026-02-24 14:52:23 +0100

    pricing section done with 2 plans

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Features from "./components/Features"
 import Product from "./components/Product"
 import Statistics from "./components/Statistics"
 import Testimonials from "./components/Testimonials"
+import Pricing from "./components/Pricing"
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Product />
       <Statistics />
       <Testimonials />
+      <Pricing />
     </>
   )
 }
